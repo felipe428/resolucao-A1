d7dacaaf956288b922631cdfa25c111cfde6ffd2
felipe428 <felipe.silva428@fatec.sp.gov.br>
2021-10-08 20:54:44 -0300

    components-criado

---
+++ src/app/felipe-currency/felipe-currency.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-felipe-currency',
+  templateUrl: './felipe-currency.component.html',
+  styleUrls: ['./felipe-currency.component.css']
+})
+export class FelipeCurrencyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/felipe-wallet/felipe-wallet.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-felipe-wallet',
+  templateUrl: './felipe-wallet.component.html',
+  styleUrls: ['./felipe-wallet.component.css']
+})
+export class FelipeWalletComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
